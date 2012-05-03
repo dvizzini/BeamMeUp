36fb365495949647c4a548d0da413fbdc8c9a385
Daniel Vizzini <dvizzini@gmail.com>
2012-05-02 19:56:21 -0700

    Revert "modifying README"
    This reverts commit d51f00587bc000b91af636105f2c962da9edb826.

--- functions.php
+++ functions.php
@@ -301,11 +301,31 @@ function beam_after_save_item($item)
 		}
 		else
 		{
+
+			curl_exec(getMetadataCurlObject(TRUE));
 			
-	        $jobDispatcher = Zend_Registry::get('job_dispatcher');
-	        $jobDispatcher->setQueueName('uploads');
-	        $jobDispatcher->send('Upload_Job', array());
-       
+			while(preg_replace('/\s/','', file_get_contents('http://archive.org/metadata/'.getBucketName())) == '{}')
+			{
+				usleep ( 1000 );
+			}
+
+			//now that bucket has been created, run multi-threaded cURL
+			$curlMultiHandle = curl_multi_init();
+			
+			$i = 0;
+			while(loop_files_for_item())
+			{
+				$curl[$i] = addHandle($curlMultiHandle,getFileCurlObject(TRUE,get_current_file()));
+			}		
+					
+			execMultiHandle($curlMultiHandle);
+			
+			for ($i = 0;$i < count($curl); $i++)//remove the handles
+			{
+				curl_multi_remove_handle($curlMultiHandle,$curl[$i]);
+			}
+			
+			curl_multi_close($curlMultiHandle);
 		}		
 	}
 }
@@ -366,46 +386,4 @@ function listInternetArchiveLinks()
 		<div>You can view the upload's Internet Archive history and progress <b><a href=http://archive.org/catalog.php?history=1&identifier=".getBucketName()." target=\"_blank\">here</a></b>.</div>
 		</br>
 	";
-}
-
-class Upload_Job extends Omeka_JobAbstract
-{
-    private $_memoryLimit;
-    private $_harvestId;
-
-    public function perform()
-    {
-		
-		curl_exec(getMetadataCurlObject(TRUE));
-		
-		while(preg_replace('/\s/','', file_get_contents('http://archive.org/metadata/'.getBucketName())) == '{}')
-		{
-			usleep ( 1000 );
-		}
-
-		//now that bucket has been created, run multi-threaded cURL
-		$curlMultiHandle = curl_multi_init();
-		
-		$i = 0;
-		while(loop_files_for_item())
-		{
-			$curl[$i] = addHandle($curlMultiHandle,getFileCurlObject(TRUE,get_current_file()));
-		}		
-				
-		execMultiHandle($curlMultiHandle);
-		
-		for ($i = 0;$i < count($curl); $i++)//remove the handles
-		{
-			curl_multi_remove_handle($curlMultiHandle,$curl[$i]);
-		}
-		
-		curl_multi_close($curlMultiHandle);
-	
-    }
-
-    public function setHarvestId($id)
-    {
-        $this->_harvestId = $id;
-    }
-    
 }
